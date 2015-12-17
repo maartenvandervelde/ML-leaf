7dde01da83c35b8a26f71e6e2427021e3940fc74
Olaf Visker <Olaf Visker>
2015-12-17 12:31:43 +0100

    Updated feature extractor; can now specify max num of keypoints

--- FeatEx.py
+++ FeatEx.py
@@ -4,10 +4,14 @@ import os
 import glob
 import numpy as np
 
-def update_progress(current, total):
+def update_progress(current, total, maxNumKeypoints):
     print('Info: Processed [{0}/{1}]'.format(current, total))
     if (current == total):
         print('Info: Processing done!')
+        if not maxNumKeypoints == 0:
+            print('Info: {0} data points written.'.format(maxNumKeypoints * 128))
+        else:
+            print('Info: No maximum number of keypoints was specified so there may be discrepancies.')
 
 def write_to_file(name, dir, data):
     file = open(dir + '/' + name + '.txt', 'w')
@@ -16,8 +20,11 @@ def write_to_file(name, dir, data):
     file.write(clean_data)
     file.close()
 
-def extract_features(img):
+def extract_features(img, maxNumKeypoints = 0):
     sift = cv2.SIFT()
+    if not maxNumKeypoints == 0:
+        sift = cv2.SIFT(maxNumKeypoints)
+
     kps, des = sift.detectAndCompute(img, None)
     return des.flatten();
 
@@ -44,10 +51,13 @@ def main(argv):
         index = 0
         for file in files:
             img = convert_to_grayscale(dir + '/' + file)
-            features = extract_features(img)
+            maxNKey = 0
+            if argc > 2:
+                maxNKey = int(argv[2])
+            features = extract_features(img, maxNKey)
             write_to_file(os.path.splitext(file)[0], dir, features)
             index = index + 1
-            update_progress(index, len(files))
+            update_progress(index, len(files), maxNKey)
 
         if (len(files) == 0):
             print('Warning: Could not find images of type png, jpg, jpeg in directory!')
