20f41f030dffcd7e8a5392a2a656ba66e2129c84
Koiter <joram.koiter@gmail.com>
2016-01-07 19:34:46 +0100

    relative path werkend gemaakt

--- SimpleNeuralNetwork.py
+++ SimpleNeuralNetwork.py
@@ -86,8 +86,7 @@ def load_weights(filedir):
 
 # returns name of class
 def result_to_string(result):
-    path = os.path.realpath('..')
-    classes = [line.rstrip('\n') for line in open(path + 'classes.txt')]
+    classes = [line.rstrip('\n') for line in open(os.path.realpath('classes.txt'))]
     maxIndex = result.index(max(result))
     outputName = classes[maxIndex]
     return outputName
@@ -110,8 +109,7 @@ if __name__ == "__main__":
     #TODO: Laad een 2D matrix (zoals op regel 132 hardcoded) van outputs
     #TODO: Krijg je leaftype voor een bepaalde file(staat hier onder)
     def returnSoort(imagename):
-        path = os.path.realpath('..')
-        f = open(path + 'imageclef_testwithgroundtruthxml.csv')
+        f = open(os.path.realpath('imageclef_testwithgroundtruthxml.csv'))
         csv_f = csv.reader(f)
         for row in csv_f:
             if row[0].find(imagename) > 0:
@@ -119,8 +117,7 @@ if __name__ == "__main__":
                 break
     #TODO: Zet deze leaftype om naar een array [0, 0, 0, 0, 1, 0, 0] (staat hier onder)
     def inputNN(leaftype):
-        path = os.path.realpath('..')
-        classes = [line.rstrip('\n') for line in open(path + 'classes.txt')]
+        classes = [line.rstrip('\n') for line in open(os.path.realpath('classes.txt'))]
         outputArray = []
         for i in xrange(0,len(classes)):
             outputArray.append(0)
